87e9f3f4d294a5ab7d279cb5b51e99198c916c46
Kamel Bourahla <kbourahla@sofgensavetax.com>
2019-04-18 09:33:01 +0200

    Update jsx

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (<div>
+    <label className="label" htmlFor="name">
+        Enter Name:
+    </label>
+    <input id="name" type="text" />
+    <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+  </div>);
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
